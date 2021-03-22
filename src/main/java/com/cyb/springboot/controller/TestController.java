748b5b40eaf790db4f52047ef5568513ebdaa12c
chenyanbing <chenyanbing_1@163.com>
2021-03-22 17:11:41 +0800

    Initial Commit

---
+++ src/main/java/com/cyb/springboot/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.cyb.springboot.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("test")
+public class TestController {
+
+    @RequestMapping("test")
+    @ResponseBody
+    public String test(){
+        return "成功接收到请求！";
+    }
+}
+
