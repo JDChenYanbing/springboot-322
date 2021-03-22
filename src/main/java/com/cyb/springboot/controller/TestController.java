cb447b9be95393e80862b048d5462f1e659e2f5d
chenyanbing <chenyanbing_1@163.com>
2021-03-22 17:15:38 +0800

    test up

--- src/main/java/com/cyb/springboot/controller/TestController.java
+++ src/main/java/com/cyb/springboot/controller/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("test2")
+@RequestMapping("test22")
 public class TestController {
 
     @RequestMapping("test")
